7894f5030629ec8eb2984b2fd6f8abf7906144b9
vimalvijaykumar <vraj3573@gmail.com>
2025-06-21 14:00:43 +0530

    Before rebase

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-blue-600">Tailwind is working 🎉</h1>
+  return <h1 className="text-3xl font-bold text-blue-600">CONTRACTOR QUOTTATION</h1>
 }
